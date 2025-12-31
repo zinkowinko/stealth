b2716cb6358a257e36ae6638df9e155f05baa35f
zinko <projectrez69420@gmail.com>
2025-12-30 17:19:49 -0800

    Implement stock handler with GET and POST methods

---
+++ stock.js
@@ -0,0 +1,15 @@
+let stock = 12;
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json({ stock });
+  } else if (req.method === "POST") {
+    if (stock <= 0) {
+      return res.status(400).json({ error: "Out of stock" });
+    }
+    stock -= 1;
+    res.status(200).json({ stock });
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+}
